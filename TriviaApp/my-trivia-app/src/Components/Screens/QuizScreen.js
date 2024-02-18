4b02aaae9cf08a9a9fcf7a2a28bacf32fbb0da8d
jack-king1 <jackking.gm97@gmail.com>
2024-02-18 18:48:11 +0000

    ""

--- TriviaApp/my-trivia-app/src/Components/Screens/QuizScreen.js
+++ TriviaApp/my-trivia-app/src/Components/Screens/QuizScreen.js
@@ -10,17 +10,18 @@ function QuizScreen(props) {
     //add context here.
     return (
         <div className="w-full h-full flex flex-col">
-            <div className="grid grid-cols-4 gap-2"></div>
             <div className="col-span-4 text-start text-2xl flex justify-between mb-6">
                 <div className="text-4xl">
                     Q:{triviaContext.playerQuestionCount + 1}{" "}
-                    {triviaContext.GetCurrentQuestion().category}
+                    {triviaContext.decodeHtml(
+                        triviaContext.GetCurrentQuestion().category
+                    )}
                 </div>
                 <div className="text-3xl font-bold">
                     Score: {triviaContext.score}
                 </div>
             </div>
-            <div className="col-span-4 text-5xl pt-4 text-center">
+            <div className="col-span-4 text-5xl pt-4 text-center mb-12">
                 {triviaContext.decodeHtml(
                     triviaContext.GetCurrentQuestion().question
                 )}
